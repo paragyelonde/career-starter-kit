ada1a9bd83abb33bc67660a9d59ff111ec2e95e8
parag.yelonde159@gmail.com <parag.yelonde159@gmail.com>
2020-09-27 18:49:23 +0530

    Added html project

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ echo "Package File Created."
 
 #uploading file
 aws s3 cp "./target/java-maven-demo-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/
+
+#create package file
+cd ../TodoApp
+mvn clean package
+echo "Package File Created."
+aws s3 cp "./target/todo-app-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/
