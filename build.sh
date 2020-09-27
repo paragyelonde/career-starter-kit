854591ded0a84e9ecdb41f724e90626ec9782d29
parag.yelonde159@gmail.com <parag.yelonde159@gmail.com>
2020-09-27 18:54:28 +0530

    Grant public acces s3

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ mvn clean package
 echo "Package File Created."
 
 #uploading file
-aws s3 cp "./target/java-maven-demo-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/
+aws s3 cp "./target/java-maven-demo-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
 #create package file
 cd ../TodoApp
 mvn clean package
 echo "Package File Created."
-aws s3 cp "./target/todo-app-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/
+aws s3 cp "./target/todo-app-1.0-SNAPSHOT.tar.gz" s3://mmkcollegebucket/parag/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
